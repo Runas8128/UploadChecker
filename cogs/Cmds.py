91682edc70aa0dadfe872aa1285a27670357b5e1
Runas <minseongexcalibur@naver.com>
2021-09-11 13:14:27 +0900

    Change Code repo from replit to github

--- cogs/Cmds.py
+++ cogs/Cmds.py
@@ -13,7 +13,7 @@ class CmdCog(commands.Cog):
 
     @commands.command(name='코드보기')
     async def CMD_ViewCode(self, ctx: commands.Context):
-        await ctx.send('https://replit.com/@Runas8128/UploadChecker')
+        await ctx.send('https://github.com/Runas8128/UploadChecker')
     
     @commands.command(name='밀린양')
     async def CMD_LeftRequest(self, ctx: commands.Context):
